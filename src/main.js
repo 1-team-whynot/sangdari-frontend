1b35b190c33801731cf7ad43c189ba3ff6be4b5c
7une9ro <racoon121238@gmail.com>
2026-06-16 10:59:59 +0900

    초기 세팅

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './routes/router'
+import App from './App.vue'
+
+createApp(App)
+  .use(router)
+  .use(createPinia())
+  .mount('#app');
